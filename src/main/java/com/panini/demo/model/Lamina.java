1a5a9b07e7aa762ea008af88d6e4e4a434282868
jairoRdgz <jairitox2000@gmail.com>
2022-05-20 13:54:19 -0500

    Warning eliminated

--- src/main/java/com/panini/demo/model/Lamina.java
+++ src/main/java/com/panini/demo/model/Lamina.java
@@ -4,12 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table (name = "laminas")
