e4da09937c971ad4b93668a84648c73ce02bac1e
jairoRdgz <jairitox2000@gmail.com>
2022-05-31 20:55:26 -0500

    list and get tests

--- src/test/java/com/panini/components/GetAlbumTest.java
+++ src/test/java/com/panini/components/GetAlbumTest.java
@@ -1,5 +1,6 @@
 package com.panini.components;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.panini.demo.model.Album;
 import com.panini.demo.model.User;
 import com.panini.demo.repository.AlbumRepository;
@@ -26,9 +27,14 @@ import lombok.var;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
+import org.everit.json.schema.Schema;
+import org.everit.json.schema.loader.SchemaLoader;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {
-        "spring.config.additional-location=classpath:component-test.yml" })
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {"spring.config.additional-location=classpath:component-test.yml" })
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @AutoConfigureMockMvc
 public class GetAlbumTest {
@@ -49,7 +55,8 @@ public class GetAlbumTest {
         User user = new User();
 
         Album album = new Album(1235478, "Birmano");
-
+        user.setUserid("usuario1");
+        user.setUsername("usuario1");
         user.addAlbum(album);
         album.setUserref(user.getUserid());
         userRepository.save(user);
@@ -61,10 +68,46 @@ public class GetAlbumTest {
 
     @Test
     @SneakyThrows
-    public void animaDetailWithSuccessStatusCodeAndContentType() throws Exception {
-        var response = (var) mockMvc.perform(get("/api/users/")).andReturn().getResponse();
+    public void albumsWithSuccessStatusCode() throws Exception {
+    	
+        var response = (var) mockMvc.perform(get("/api/users/usuario1/albums/11824")).andReturn().getResponse();
 
         assertThat(((MockHttpServletResponse) response).getStatus(), equalTo(HttpStatus.OK.value()));
         assertThat(((MockHttpServletResponse) response).getContentType(), equalTo(MediaType.APPLICATION_JSON.toString()));
+    	
+    }
+    
+    @Test
+    @SneakyThrows
+    public void detailedAlbumWithSuccessStatusCode() throws Exception {
+    	var response = (var) mockMvc.perform(get("/api/users/usuario1/albums/11824")).andReturn().getResponse();
+    	Album album =  new ObjectMapper().readValue(((MockHttpServletResponse) response).getContentAsString(), Album.class);
+    	
+    	assertThat(album.getAlbumName(), equalTo("Birmano"));
+    	assertThat(album.getAlbumid(), equalTo(11824));
+    	assertThat(album.getUserref(), equalTo("usuario1"));
+    }
+    
+    @Test
+    @SneakyThrows
+    public void listAlbums() throws Exception {
+    	var response = (var) mockMvc.perform(get("/api/users/usuario1/albums/")).andReturn().getResponse();
+
+        assertThat(((MockHttpServletResponse) response).getStatus(), equalTo(HttpStatus.OK.value()));
+        assertThat(((MockHttpServletResponse) response).getContentType(), equalTo(MediaType.APPLICATION_JSON.toString()));
+    }
+    	
+   
+    @Test
+    @SneakyThrows
+    public void listAlbumsWithRIgthSchema() throws Exception {
+    	var response = (var) mockMvc.perform(get("/api/users/usuario1/albums/")).andReturn().getResponse();
+    	
+    	var jsonSchema = (var) new JSONObject(new JSONTokener(GetAlbumTest.class.getResourceAsStream("/api/users/usuario1/albums.json")+""));
+        var jsonArray = (var) new JSONArray(((MockHttpServletResponse) response).getContentAsString());
+
+        var schema = (var) SchemaLoader.load((JSONObject) jsonSchema);
+        ((Schema) schema).validate(jsonArray);
+    	
     }
 }
